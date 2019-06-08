8ca46bbc71d1c2431048aa9bb07236757b45d96d
Peyton Tettleton <peytontettleton@me.com>
2019-06-08 14:05:07 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Peyton Tettleton on 6/8/19.
+//  Copyright © 2019 Peyton Tettleton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
